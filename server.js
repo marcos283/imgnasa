c0f4d04f77c047886be7050a787bd729116113fc
Marcos Valverde Gonzalez <76835617+marcos283@users.noreply.github.com>
2025-03-27 17:48:50 +0100

    Modificar server.js y script.js para inyectar variables de entorno

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'), {
+    NASA_API_KEY: process.env.NASA_API_KEY,
+    GOOGLE_TRANSLATE_API_KEY: process.env.GOOGLE_TRANSLATE_API_KEY
+  });
 });
 
 app.listen(port, () => {
