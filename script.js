3652d1ec65fdcac0a46d5dd4f88fd6c945203ed1
Marcos Valverde Gonzalez <76835617+marcos283@users.noreply.github.com>
2025-03-27 17:30:52 +0100

    Actualizar archivos para manejar claves de API en el navegador

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Constantes globales
 const NASA_APOD_URL = "https://api.nasa.gov/planetary/apod";
-const nasaApiKey = process.env.NASA_API_KEY;
-const googleTranslateApiKey = process.env.GOOGLE_TRANSLATE_API_KEY;
+const nasaApiKey = NASA_API_KEY;
+const googleTranslateApiKey = GOOGLE_TRANSLATE_API_KEY;
 
 // Variables globales para elementos del DOM
 let imageContainer;
